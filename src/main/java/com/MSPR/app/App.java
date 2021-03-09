139884383e355372d83a9101c64d9e3a065ea76a
Sheiicker <shadow.mic89@yahoo.com>
2021-03-09 10:42:33 +0100

    Base

---
+++ src/main/java/com/MSPR/app/App.java
@@ -0,0 +1,13 @@
+package com.MSPR.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
